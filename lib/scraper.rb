a8529bd72f54ab02316aa9a9d708c561041911a5
endotnick <endotnick@gmail.com>
2018-12-14 19:45:13 -0800

    clean scraper.rb

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,11 +4,11 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     Array.new.tap do |data|
-      Nokogiri::HTML(File.open(index_url)).css('div.student-card > a').each do |student|
+      Nokogiri::HTML(open(index_url)).css('div.student-card a').each do |student|
         data << {
           name:  student.css('h4.student-name').text,
           location: student.css('p.student-location').text,
-          profile_url: student['href']
+          profile_url: student['href'],
         }
       end
     end
@@ -16,22 +16,22 @@ class Scraper
 
   def self.scrape_profile_page(profile_url)
     social_media = {}
-    profile = Nokogiri::HTML(File.open(profile_url))
+    profile = Nokogiri::HTML(open(profile_url))
     targeted_platforms = [:twitter, :linkedin, :github, :blog]
 
-    #get social
-    profile.css('.social-icon-container > a').each do |social|
+    # get social
+    # this assigns instagram, facebook etc to :blog,
+    # can't have those keys w/ this test suite.
+    profile.css('.social-icon-container a').each do |social|
       domain = social['href'][/\/\/w*\.*(.+)\.com/, 1].to_sym
-      # this assigns instagram, facebook etc to :blog,
-      # can't have those keys w/ this test suite.
       sym = targeted_platforms.include?(domain) ? domain : :blog
       social_media[sym] = social['href']
     end
 
-    #build data hash
+    # build data hash
     data = {
       profile_quote: profile.css('.profile-quote').text,
-      bio: profile.css('.bio-content > .description-holder > p').text,
+      bio: profile.css('.bio-content .description-holder p').text,
     }    
     targeted_platforms.each { |target| data[target] = social_media[target] }
     data.delete_if { |k,v| v.nil? }
